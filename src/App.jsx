1148ae1b8c8463f5494370f52869c2dc6f3c2a15
JDGipson <jdgipson1989@gmail.com>
2023-06-13 12:42:37 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+
+  const COHORT_NAME = "2304-FTB-ET-WEB-FT";
+  const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
+  const 
+
+  return <></>;
+}
+
+export default App;
