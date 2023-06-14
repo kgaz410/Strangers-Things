cefbe717372c1b701144e9760eb4fa90fa8cd599
JDGipson <jdgipson1989@gmail.com>
2023-06-14 14:40:40 -0500

    updated handle submit typo in login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,100 +2,80 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 const COHORT_NAME = "2304-FTB-ET-WEB-FT";
 const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
-
-
-
 function Login(props) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    // useNavigate('/posts');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  // useNavigate('/posts');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    try {
+      const result = await loginUser(); // Passing our async function in from below.
+      console.log(result.data);
 
-
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        console.log(username, password)
-        try {
-            const result = await loginUser(); // Passing our async function in from below.
-            console.log(result.data)
-
-            localStorage.getItem("token", result.data.token) // Fetching only key-value pair for token for the login.
-            props.setIsLoggedIn(true)  // Telling program login is true.
-
-        } catch (error) {
-            console.log(error)
-        }
-
+      localStorage.setItem("token", result.data.token); // Fetching only key-value pair for token for the login.
+      props.setIsLoggedIn(true); // Telling program login is true.
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-
-    async function loginUser() {
-        try {
-            const response = await fetch(`${BASE_URL}/users/login`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    user: {
-                        username: username,
-                        password: password,
-                       
-                    }
-                })
-            });  // Outside of fetch starting here.
-            const result = await response.json()
-            return result;
-        } catch (error) {
-            console.log(error)
-        }
+  async function loginUser() {
+    try {
+      const response = await fetch(`${BASE_URL}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user: {
+            username: username,
+            password: password,
+          },
+        }),
+      }); // Outside of fetch starting here.
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    
-    
-    return(
-        <div>
-            <h1>LOGIN</h1>
-        <form onSubmit={handleSubmit}>
-            <label>Username:
-                <input
-                    type="text"
-                    value={username}
-                    onChange={(e) => {
-                        console.log(e.target.value);
-                        setUsername(e.target.value);
-                    }}
-                />
-            </label>
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setUsername(e.target.value);
+            }}
+          />
+        </label>
 
-            <label>Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => {
-                        console.log(e.target.value);
-                        setPassword(e.target.value);
-                    }}
-                />
-            </label>
-            <button type="submit">Submit</button>
-
-        </form>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setPassword(e.target.value);
+            }}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-
-
-
-
-    )
+  );
 }
 
-
-
 export default Login;
-
